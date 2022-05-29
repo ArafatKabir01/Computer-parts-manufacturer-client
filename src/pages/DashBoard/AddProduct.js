c8235a5ba21f19924f81f7afb599fe3be0f3a499
ak nabil <nabil786.ak@gmail.com>
2022-05-29 20:53:28 +0600

    manage product added

--- src/pages/DashBoard/AddProduct.js
+++ src/pages/DashBoard/AddProduct.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddProduct = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
+        console.log(data)
+        const proceed = window.confirm(`you added ${data.name}?`)
+        if(proceed){
+            const url = `http://localhost:5000/parts`
+            fetch(url,{
+                method:'POST',
+                headers:{'content-type':'application/json'},
+                body: JSON.stringify(data)
+    
+            })
+            .then(res => res.json())
+            .then(result => console.log(result))
+            window.location.reload()
+
+        }
+
+    };
     return (
         <div>
-            
+            <h2 className='text-center'>Added Product</h2><br/>
+            <form className='text-center text-black ' onSubmit={handleSubmit(onSubmit)}>
+      <input type='text' {...register("name")} placeholder='Porduct Name' required/><br/><br/>
+      <input type="number" {...register("price")} placeholder='Price' required /><br/><br/>
+      <input type="number" {...register("quantity")} placeholder='Product Quantity' required /><br/><br/>
+      <input type="number" {...register("minQuantity")} placeholder='Min Quantity' required /><br/><br/>
+      <input {...register("img")} placeholder='img url' required/><br/><br/>
+      <textarea {...register("text")} placeholder='Product Description' required/><br/><br/>
+      <button class="btn btn-primary">Submit</button>
+    </form>
         </div>
     );
 };
--- src/pages/DashBoard/ManageProduct.js
+++ src/pages/DashBoard/ManageProduct.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const ManageProduct = () => {
+    const [user] = useAuthState(auth)
+    const [products, setproducts] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/parts')
+        .then(res => res.json())
+        .then(data => setproducts(data))
+    }, [user])
+    const handleDelete = id => {
+        const proceed = window.confirm('are you sure?')
+        console.log(id)
+        if (proceed) {
+            const url = `http://localhost:5000/allProduct/${id}`
+            fetch(url, {
+                method: "DELETE"
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    const remaining = products.filter(order => order._id !== id);
+                    setproducts(remaining);
+                })
+
+        }
+    }
     return (
         <div>
-            
-        </div>
-    );
+            <h2 className='p-5'>MyOrder {products.length}</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th>Component Name</th>
+                            <th>Price Per Unit</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            products.map(product => <tr class="hover"><td>{product.name}</td><td>{product.price}</td><td><button onClick={() => handleDelete(product._id)} className="btn btn-primary">Delete</button></td></tr>)
+
+                        }
+                    </tbody>
+
+
+
+                </table>
+            </div>
+            </div>
+            );
 };
 
-export default ManageProduct;
+            export default ManageProduct;
